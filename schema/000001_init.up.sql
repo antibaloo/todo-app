499b0a1e6c5c46f0a7dc0b978d1bdc5ced6440c0
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-18 22:48:50 +0500

    Небольшие исправления в миграции

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,31 +1,31 @@
 CREATE TABLE users {
-    id              serial not null unique,
+    id              serial primary key,
     name            varchar(255) not null,
-    usename         varchar(255) not null unique,
+    username        varchar(255) not null unique,
     password_hash   varchar(255) not null
 }
 
 CREATE TABLE todo_lists{
-    id serial not nul unique,
-    title varchar(255) not null,
+    id          serial primary key,
+    title       varchar(255) not null,
     description varchar(255)
 }
 
 CREATE TABLE users_lists{
-    id serial not null unique,
+    id      serial primary key,
     user_id int references users(id) on delete cascade not null,
     list_id int references todo_lists(id) on delete cascade not null
 }
 
 CREATE TABLE todo_items{
-    id serial not null unique,
-    title varchar(255) not null,
+    id          serial primary key,
+    title       varchar(255) not null,
     dercription varchar(255)
-    done boolean not null default false
+    done        boolean not null default false
 }
 
 CREATE TABLE lists_items{
-    id serial not null unique,
+    id      serial primary key,
     item_id int references todo_items(id) on delete cascade not null,
     list_id int references todo_lists(id) on delete cascade not null
 }
