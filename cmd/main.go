8504b9d4954be3d98dbfa5c6211482ceefdedf19
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-19 22:42:12 +0500

    Убираем из конфига пароль от БД, так делать нельзя. Переносим пароль в файл .env в корне проекта в переменную DB_PASSWORD и загружаем его перед инициализацие подключения к БД. В этом учебном проекте это не страшно, т.к. база с локальным доступом исключительно, но в боевых проектах или при работе с БД доступной из вне, никогда не оставляйте авторизационную информацию в публичных репозиториях.

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"os"
 	"todo-app"
 	"todo-app/pkg/handler"
 	"todo-app/pkg/repository"
 	"todo-app/pkg/service"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 
 	"github.com/spf13/viper"
@@ -17,11 +19,15 @@ func main() {
 		log.Fatalf("error initilizing config: %s", err.Error())
 	}
 
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
 	db, err := repository.NewPostgresDB(repository.Config{
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
 		User:     viper.GetString("db.user"),
-		Password: viper.GetString("db.password"),
+		Password: os.Getenv("DB_PASSWORD"),
 		DBName:   viper.GetString("db.dbname"),
 		SSLMode:  viper.GetString("db.sslmode"),
 	})
