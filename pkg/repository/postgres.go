2c1b6615baee9af89170ca731f66571515475ef9
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-19 22:13:26 +0500

    Новый файл пакета repository с методом NewPostgresDB для создания подключения к базе данных postgresql и структурой кофигурации подключения

---
+++ pkg/repository/postgres.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+// NewPostgresDB ...
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open(
+		"postgres",
+		fmt.Sprintf(
+			"host = %s port = %s user = %s password = %s dbname = %s sslmode = %s",
+			cfg.Host,
+			cfg.Port,
+			cfg.User,
+			cfg.Password,
+			cfg.DBName,
+			cfg.SSLMode,
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
