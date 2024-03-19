dc8a37e30d816844e814bc786742c52ae8a9b9c8
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-20 01:21:53 +0500

    Реализован метод обработки запроса на регистрацию пользователя: с проверкой обязательных полей, вызывающий метод создания записи в таблице пользователей в БД и возвращающий результат работы этого метода в ответ на вызов обработчика

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,10 +1,30 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"todo-app"
 
+	"github.com/gin-gonic/gin"
+)
+
+// signUp ...
 func (h *Handler) signUp(c *gin.Context) {
-
+	var input todo.User
+	if err := c.BindJSON(&input); err != nil {
+		NewErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	id, err := h.services.CreateUser(input)
+	if err != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
+
+// signIn ...
 func (h *Handler) signIn(c *gin.Context) {
 
 }
