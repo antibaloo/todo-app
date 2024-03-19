44fd2c4fa822a2da40e108cd7ee331552db0a697
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-20 01:17:56 +0500

    Добавлена структура хранилища для авторизации, конструктор этой структуры и реализован метод CreateUser, создающий новую запись в таблице пользователя и возвращающий ошибку создания или идентификатор созданного пользователя

---
+++ pkg/repository/auth_postgres.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"fmt"
+	"todo-app"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type AuthPostgres struct {
+	db *sqlx.DB
+}
+
+// NewAuthPostgres ...
+func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
+	return &AuthPostgres{db: db}
+}
+
+func (r *AuthPostgres) CreateUser(user todo.User) (int, error) {
+	var id int
+	query := fmt.Sprintf("INSERT into %s (name, username, password_hash) VALUES ($1, $2, $3) RETURNING id", usersTable)
+	row := r.db.QueryRow(query, user.Name, user.Username, user.Password)
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+	return id, nil
+}
