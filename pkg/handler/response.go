2f9a0992f4f22983cd20b3cf93449e70b109ed50
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-20 01:19:25 +0500

    Реализована структура с сообщением об ошибке и метод возвращающий код ошибки и сообщение, как ответ обработчика

---
+++ pkg/handler/response.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+type error struct {
+	Message string `json:"message"`
+}
+
+// NewErrorResponse ...
+func NewErrorResponse(c *gin.Context, statusCode int, message string) {
+	logrus.Error(message)
+	c.AbortWithStatusJSON(statusCode, error{message})
+
+}
