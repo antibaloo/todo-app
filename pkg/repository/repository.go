bc7c5cec99ce9fd1adee9eba83872fac7244ef73
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-19 22:14:34 +0500

    При вызове метода создания репозитория теперь в него передается указатель на подключение в БД

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Authorization interface {
 }
 
@@ -15,6 +17,7 @@ type Repository struct {
 	TodoItem
 }
 
-func NewRepository() *Repository {
+// NewRepository ...
+func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{}
 }
