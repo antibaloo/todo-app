11f6c3ec76d4bca4c891cb8aa95bffbc1916ce3b
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-20 23:13:46 +0500

    Добавлено описание метода Create в интерфейс TodoList, конструктор дополнен инициализацией дополнительного интерфейса

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -12,6 +12,7 @@ type Authorization interface {
 }
 
 type TodoList interface {
+	Create(userId int, list todo.TodoList) (int, error)
 }
 
 type TodoItem interface {
@@ -27,5 +28,6 @@ type Repository struct {
 func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{
 		Authorization: NewAuthPostgres(db),
+		TodoList:      NewTodoListPostgres(db),
 	}
 }
