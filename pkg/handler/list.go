62a8a779802a82592f9056dfb2d6530f87434382
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-20 23:08:04 +0500

    Имплементирован метод для создания списка: получение идентификатора пользователя из контекста, проверка правильности структуры запроса, создание списка, проверка ошибки, возврать идентификатора списка в случае успеха

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -2,12 +2,33 @@ package handler
 
 import (
 	"net/http"
+	"todo-app"
 
 	"github.com/gin-gonic/gin"
 )
 
+// createList ...
 func (h *Handler) createList(c *gin.Context) {
-	id, _ := c.Get(userCtx)
+	// получаем идентификатор пользователя из контекста
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var input todo.TodoList
+	// проверяем правильность структуры запроса
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// создаем список
+	id, err := h.services.TodoList.Create(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	// возвращаем идентификатор созданного списка
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
