4a8fa2e5eb39f495dceda3dfb847832e6343b474
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-20 13:03:37 +0500

    Добавлены константы: случайна строка для подписи токена и вредмя жизни токена. Добавлена структура для генерации jwt-токена: к стандарто-ной обвязке добавлен индентификатор пользователя. Добавлен метод генерации jwt-токена на основании имени пользователя и паароля

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,12 +3,24 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 	"todo-app"
 	"todo-app/pkg/repository"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "fbsdkjcsdkbskbgk"
+const (
+	salt       = "fbsdkjcsdkbskbgk"
+	signingKey = "sjkbfuhuiowreiuoivopkjonjtfjbserb"
+	tokenTTL   = 12 * time.Hour
+)
 
+// Дополненная структура tokenClaims
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 type AuthService struct {
 	repo repository.Authorization
 }
@@ -22,6 +34,22 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = s.generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+// GenerateToken ...
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+	return token.SignedString([]byte(signingKey))
 }
 
 // generatePasswordHash ...
