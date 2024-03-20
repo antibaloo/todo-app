a9eafca631c7dab97ace71375d2dfb07d14232aa
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-20 12:58:43 +0500

    Добавлен обработчик запрос на аутентификацию, принимает обновленную структуру с логином и паролем, в случае удачно аутентификации возвращает jwt-токен для авторизации при последующих операциях

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -24,7 +24,25 @@ func (h *Handler) signUp(c *gin.Context) {
 	})
 }
 
-// signIn ...
-func (h *Handler) signIn(c *gin.Context) {
+// Структура для авторизации, структура для регистрации не подойдет, т.к. имя пользователя в ней обязательный параметр
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
+// Обработчик запроса аутентификации ...
+func (h *Handler) signIn(c *gin.Context) {
+	var input signInInput
+	if err := c.BindJSON(&input); err != nil {
+		NewErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
