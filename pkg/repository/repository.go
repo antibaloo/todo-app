fb9fbe2a00e44cfef5a8ee83612ab9bb1f4ef29c
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-21 23:26:23 +0500

    Добавлены описания новых методов в интерфес хранилища, в конструкторе инициализирован новыый интерфейс.

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -20,6 +20,8 @@ type TodoList interface {
 }
 
 type TodoItem interface {
+	Create(listId int, item todo.TodoItem) (int, error)
+	GetAll(userId, listId int) ([]todo.TodoItem, error)
 }
 
 type Repository struct {
@@ -33,5 +35,6 @@ func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{
 		Authorization: NewAuthPostgres(db),
 		TodoList:      NewTodoListPostgres(db),
+		TodoItem:      NewTodoItemPostgres(db),
 	}
 }
