d3b1e8dcf818fc37fa8969392aab6dfaa9fe0663
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-22 12:48:45 +0500

    Имплементируем новые методы, в которых пробрасываем вызов на уровень ниже, в хранилище

--- pkg/service/todo_item.go
+++ pkg/service/todo_item.go
@@ -29,3 +29,15 @@ func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, e
 func (s *TodoItemService) GetAll(userId, listId int) ([]todo.TodoItem, error) {
 	return s.repo.GetAll(userId, listId)
 }
+
+func (s *TodoItemService) GetById(userId, itemId int) (todo.TodoItem, error) {
+	return s.repo.GetById(userId, itemId)
+}
+
+func (s *TodoItemService) Delete(userId, itemId int) error {
+	return s.repo.Delete(userId, itemId)
+}
+
+func (s *TodoItemService) Update(userId, itemId int, input todo.UpdateItemInput) error {
+	return s.repo.Update(userId, itemId, input)
+}
