70ceb8cd9542326b50c63769a636d18eef36359a
Andrey Abalakov <a.s.abalakov@yandex.ru>
2024-03-22 12:49:37 +0500

    Добавляем описание новых методов в интерфейс хранилища по работе с элементами списка

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -22,6 +22,9 @@ type TodoList interface {
 type TodoItem interface {
 	Create(listId int, item todo.TodoItem) (int, error)
 	GetAll(userId, listId int) ([]todo.TodoItem, error)
+	GetById(userId, itemId int) (todo.TodoItem, error)
+	Delete(userId, litemId int) error
+	Update(userId, itemId int, input todo.UpdateItemInput) error
 }
 
 type Repository struct {
